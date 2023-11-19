df12f88a539682b59968a4fbb254255964e94c3a
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2023-11-19 14:31:44 -0600

    chore: initial action implementation

--- dist/index.js
+++ dist/index.js
@@ -2752,23 +2752,33 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = void 0;
+const promises_1 = __nccwpck_require__(977);
 const core = __importStar(__nccwpck_require__(186));
-const wait_1 = __nccwpck_require__(259);
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 async function run() {
     try {
-        const ms = core.getInput('milliseconds');
-        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        core.debug(`Waiting ${ms} milliseconds ...`);
-        // Log the current timestamp, wait, then log the new timestamp
-        core.debug(new Date().toTimeString());
-        await (0, wait_1.wait)(parseInt(ms, 10));
-        core.debug(new Date().toTimeString());
-        // Set outputs for other workflow steps to use
-        core.setOutput('time', new Date().toTimeString());
+        const path = core.getInput('path');
+        core.debug(`Locating ${path} ...`);
+        const file = await (0, promises_1.readFile)(path, { encoding: 'utf8' });
+        if (!file) {
+            throw Error(`Unable to locate ${path}.`);
+        }
+        const config = JSON.parse(file);
+        let output;
+        if (config.type === 'RESUME') {
+            output = [
+                '<ul>',
+                ...config.contents.experience?.map((e) => `<li>${e.title} &mdash; ${e.company_name} &mdash; ${e.location}</li>`),
+                '</ul>'
+            ];
+        }
+        if (config.type === 'RESEARCH_DATA_PORTAL') {
+            output = `@todo`;
+        }
+        if (!output) {
+            throw Error(`No renderer found for type: ${config.type}`);
+        }
+        await (0, promises_1.mkdir)('out');
+        await (0, promises_1.writeFile)('out/index.html', config.version);
     }
     catch (error) {
         // Fail the workflow run if an error occurs
@@ -2777,31 +2787,6 @@ async function run() {
     }
 }
 exports.run = run;
-
-
-/***/ }),
-
-/***/ 259:
-/***/ ((__unused_webpack_module, exports) => {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-/**
- * Wait for a number of milliseconds.
- * @param milliseconds The number of milliseconds to wait.
- * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-    return new Promise(resolve => {
-        if (isNaN(milliseconds)) {
-            throw new Error('milliseconds not a number');
-        }
-        setTimeout(() => resolve('done!'), milliseconds);
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
@@ -2859,6 +2844,14 @@ module.exports = require("https");
 
 "use strict";
 module.exports = require("net");
+
+/***/ }),
+
+/***/ 977:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:fs/promises");
 
 /***/ }),
 
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,44 @@
-import * as core from '@actions/core'
-import { wait } from './wait'
+import { readFile, mkdir, writeFile } from 'node:fs/promises'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
+import * as core from '@actions/core'
+
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const path: string = core.getInput('path')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Locating ${path} ...`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const file = await readFile(path, { encoding: 'utf8' })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    if (!file) {
+      throw Error(`Unable to locate ${path}.`)
+    }
+
+    const config = JSON.parse(file)
+
+    let output
+    if (config.type === 'RESUME') {
+      output = [
+        '<ul>',
+        ...config.contents.experience?.map(
+          (e: any) =>
+            `<li>${e.title} &mdash; ${e.company_name} &mdash; ${e.location}</li>`
+        ),
+        '</ul>'
+      ]
+    }
+
+    if (config.type === 'RESEARCH_DATA_PORTAL') {
+      output = `@todo`
+    }
+
+    if (!output) {
+      throw Error(`No renderer found for type: ${config.type}`)
+    }
+
+    await mkdir('out')
+
+    await writeFile('out/index.html', config.version)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
